9f38569352368d3a4d5aeebe8203e908782661a4
Math0XK <145477623+Math0XK@users.noreply.github.com>
2023-09-20 15:22:18 +0200

    reflex maj

--- Loadder/Lib/reflex.h
+++ Loadder/Lib/reflex.h
@@ -1,10 +1,8 @@
 #include "util.h"
 
 int val;
-int cpt;
-
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
-
+long int t1, t2, tf;
+bool buz = true;
 
 void ReflexSetup() {
 
@@ -15,8 +13,6 @@ void ReflexSetup() {
     pinMode(led_jaune, OUTPUT);
 
     pinMode(buzzer, OUTPUT);
-
-	cpt= 0;
 	val= random(4,8);
 
 }
@@ -24,23 +20,24 @@ void ReflexSetup() {
 void ReflexLoop() {
 
 	//Serial.println(val);
-	 cpt = time_t();
-
-
 	if(analogRead(val-4)>=512) {
-		cpt= time_t() - cpt;
+		t2 = millis();
+		tf = t2 - t1;
 		lcd.clear();
 		digitalWrite(val,LOW);
 		lcd.print("Vous avez mis ");
 		lcd.setCursor(0,1);
-		lcd.print(cpt);
+		lcd.print(tf);
 		lcd.print(" ms");
+		buz = true;
 		delay(random(500,2500));
 		val= random(4,8);
 	} else {
 		digitalWrite(val,HIGH);
-		if(cpt==0) PlayBuzzer((val-3)*63);
-		cpt++
-		delay(1);
+		if(buz) {
+			t1 = millis();
+			PlayBuzzer((val-3)*63);
+			}
+		buz = false;
 	}
 }
